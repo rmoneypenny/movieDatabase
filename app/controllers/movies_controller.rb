6063e03e2e476414329e8fdf5123e617448d599c
rmoneypenny <rlmoneypenny@gmail.com>
2019-07-10 01:02:37 -0400

    Movie page work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,41 @@
 class MoviesController < ApplicationController
 
 	def index
-		@test = Time.now
 		@movie = MovieAPI.new
 		@movieList = @movie.movieList
+		@categoryHide = "display: none"
 
 	end
 
 	def searchMovies
-		@test = Time.now
-		if params[:page]
-			@movie = MovieAPI.new(params[:page].to_i)
-			@movieList = @movie.movieList
+
+		if params[:search] == ""
+			@categoryHide = "display: none"
+		else
+			@categoryHide = ""
 		end
+
+		@movie = MovieAPI.new(params[:page].to_i, params[:search])
+		@movie.setCategory(params[:category])
+		@movieList = @movie.movieList
+
+		# if params[:search] == ""
+		# 	@movie = MovieAPI.new
+		# 	@movie.search = params[:search]
+		# 	@movieList = @movie.movieList
+		# 	@categoryHide = ""
+		# else
+		# 	@categoryHide = "display: none"
+		# end
+
+		# if params[:category]
+		# 	@movie = MovieAPI.new(params[:page].to_i)
+		# 	@movie.setCategory(params[:category])
+		# 	@movieList = @movie.movieList
+		# elsif params[:page]
+		# 	@movie = MovieAPI.new(params[:page].to_i)
+		# 	@movieList = @movie.movieList
+		# end
 
 		respond_to do |format|
 			format.html
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,21 @@ module ApplicationHelper
 
 		BASE_URL = "https://api.themoviedb.org/3/discover/movie"
 		API_URL = "?api_key=#{ENV['MOVIEDB_API_KEY']}"
-		SEARCH_URL = "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page="
-		RELEASE_DATE = "&release_date.gte="
+		LANGUAGE_URL = "&language=en-US"
+		RELEASEDATE_URL = "&release_date.lte="
+		END_URL = "&include_adult=false&include_video=false&page="
+		SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 
 		attr_accessor :page
+		attr_accessor :category
+		attr_accessor :search
+		#&sort_by=popularity.desc
+		#release_date.desc
+		#popularity.desc
 
-		def initialize(page = 1)
+		def initialize(page = 1, search = "")
 
-			genreURL = "https://api.themoviedb.org/3/genre/movie/list" + API_URL + "&language=en-US"
+			genreURL = "https://api.themoviedb.org/3/genre/movie/list" + API_URL + LANGUAGE_URL
 			request = HTTParty.get(genreURL).to_json
 			genreList = JSON.parse(request)
 			@genreHash = {}
@@ -18,12 +25,17 @@ module ApplicationHelper
 				@genreHash[g["id"]] = g["name"]
 			end
 			page > 0 ? (@page = page) : (@page = 1)
+			@category = "&sort_by=popularity.desc"
+			@search = search
 		end
 
 		def buildURL(page)
-			twoMonths = (DateTime.now - 1.month).strftime("%Y-%m-%d")
-			fullURL = BASE_URL + API_URL + SEARCH_URL + page.to_s# + RELEASE_DATE + twoMonths
-			
+			if @search == ""
+				fullURL = BASE_URL + API_URL + LANGUAGE_URL + END_URL + page.to_s + RELEASEDATE_URL + DateTime.now.strftime("%Y-%m-%d")
+			else
+				fullURL = SEARCH_URL + API_URL + "&query=" + @search
+			end
+
 			request = HTTParty.get(fullURL).to_json
 			@request_hash = JSON.parse(request)
 		end
@@ -51,6 +63,17 @@ module ApplicationHelper
 			genreNames
 		end
 
+		def setCategory(category)
+			case category
+			when "Title"
+				@category = "&sort_by=original_title.desc"
+			when "Release Date"
+				@category = "&sort_by=release_date.desc"
+			when "Genre"
+				@category = "&sort_by=original_title.desc"
+			end
+
+		end
 
 
 	end
