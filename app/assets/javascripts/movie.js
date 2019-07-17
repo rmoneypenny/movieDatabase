cc6e63e80cd0f980a8c2c6701c87b1664dc292fa
rmoneypenny <rlmoneypenny@gmail.com>
2019-07-17 00:16:51 -0400

    clickable star ratings

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -9,48 +9,54 @@ $(document).on("click", ".review-button", function(){
 $(document).on("click", ".one-star", function(){
 	score = 1;
 	var divID = $(this).parent().parent().attr("id");
-	html = updateStars(score);
-	document.getElementById(divID).innerHTML = html;   
+	updateStars(divID, score);
+	updateScoreField(divID, score);
 });	
 $(document).on("click", ".two-star", function(){
 	score = 2;	
 	var divID = $(this).parent().parent().attr("id");
-	html = updateStars(score);
-	document.getElementById(divID).innerHTML = html;   
+	updateStars(divID, score);
+	updateScoreField(divID, score);
 });	
 $(document).on("click", ".three-star", function(){
 	score = 3;	
 	var divID = $(this).parent().parent().attr("id");
-	html = updateStars(score);
-	document.getElementById(divID).innerHTML = html;   
+	updateStars(divID, score);
+	updateScoreField(divID, score); 
 });	
 $(document).on("click", ".four-star", function(){
 	score = 4;	
 	var divID = $(this).parent().parent().attr("id");
-	html = updateStars(score);
-	document.getElementById(divID).innerHTML = html;   
+	updateStars(divID, score);
+	updateScoreField(divID, score);
 });	
 $(document).on("click", ".five-star", function(){
 	score = 5;	
 	var divID = $(this).parent().parent().attr("id");
-	html = updateStars(score);
-	document.getElementById(divID).innerHTML = html;   
+	updateStars(divID, score);
+	updateScoreField(divID, score);  
 });	
 
-function updateStars(score){
+function updateStars(divID, score){
 
-	var html = "<button type=\"button\" class=\"btn btn-default btn-sm\">";
+	var html = "<button type=\"button\" class=\"btn btn-default btn-default\">";
+	var starNumbers = ["one-star", "two-star", "three-star", "four-star", "five-star"];
     for(i=0; i<5; i++){
 		if(i<score){
-			html += "<span class=\"glyphicon glyphicon-star star\"></span>";
+			html += "<span class=\"glyphicon glyphicon-star star star-hover " + starNumbers[i] + "\"></span>";
 		}
 		else{
-			html += "<span class=\"glyphicon glyphicon-star-empty\"></span>";
+			html += "<span class=\"glyphicon glyphicon-star-empty star-hover " + starNumbers[i] + "\"></span>";
 		}
 	};
 	html += "<br></button>";
-	return html;
+	document.getElementById(divID).innerHTML = html;   
 };
 
-// var score = $(this).parent().siblings().val();
-// <%= hidden_field_tag "score", 3 %>
+function updateScoreField(divID, score){
+	
+	var moviedbid = "score-" + divID.slice(6, divID.length);
+	document.getElementById(moviedbid).value = score;
+	
+};
+
